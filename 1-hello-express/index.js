3049da55b1eea5ef97bf9daf401caf11415aeaea
doannd <doannd@ghtk.vn>
2020-04-26 18:37:06 +0700

    initial project

---
+++ 1-hello-express/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send("Hello World!"));
+
+app.get('/list-user', (req, res) => res.send("list-user"));
+
+app.get('/create-user', (req, res) => {
+    res.send('<h1>Hello nodejs</h1>')
+})
+
+// start server
+app.listen(port, () => {
+  console.log(`My server starting at http://localhost:${port}`);
+});
